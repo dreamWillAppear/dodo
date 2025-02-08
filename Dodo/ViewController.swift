27b39a50d99321cd6fa0b687b75798b09da993ab
dreamlogin <dreamlogin@vk.team>
2025-02-08 23:42:03 +0300

    Initial Commit

---
+++ Dodo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dodo
+//
+//  Created by dreamlogin on 08.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
